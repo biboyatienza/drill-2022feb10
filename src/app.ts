4ee5fe7dc3a0e2bfe6cb2eb76a7df345ace50b8e
ca Is BACK <caisback@outlook.com>
2022-02-10 12:41:17 +0800

    Applied TS code and error handling

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,35 @@
-import express from 'express'
+import express, { Express, Request, Response, NextFunction } from 'express'
+import morgan from 'morgan'
+import createHttpError from 'http-errors'
+import dotenv from 'dotenv'
 
-const createdBy = '-- drill(2022.Feb.10) ExpressJs X'
-const app = express()
+dotenv.config()
+
+const createdBy: String = '-- drill(2022.Feb.10) ExpressJs X'
+const app: Express = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use(morgan('dev'))
 
-app.get('/check', (req, res, next) => {
+app.get('/check', (req: Request, res: Response, next: NextFunction) => {
   res.send(`All OK! ${createdBy}`)
 })
 
-const PORT = 3000
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(new createHttpError.NotFound())
+})
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status = err.status || 500,
+  res.send({
+    status: res.status,
+    message: err.message
+  })
+})
+
+const PORT: Number = Number(process.env.PORT || 3000)
 app.listen(PORT, () => {
-  console.log(`Game! ${createdBy}`)
+  console.log(`Game! at port ${PORT} ${createdBy}`)
 })
 
